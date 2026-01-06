86b41ca51c359fb73c06fbd7f16349c8c9665381
Sofia Mac <shestakova.sofia2016@mail.ru>
2026-01-07 00:38:27 +0400

    changed

--- src/app/[locale]/auth/Auth.tsx
+++ src/app/[locale]/auth/Auth.tsx
@@ -1,18 +1,26 @@
 'use client';
 
-import { useSession } from '@/lib/auth-client';
+import { signUp, useSession } from '@/lib/auth-client';
 import { useState } from 'react';
 
-export default function Auth() {
+export function Auth() {
   const { data: session, isPending } = useSession();
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [name, setName] = useState('');
 
   const handeSignUp = async () => {
-    await signUP
+    await signUp({email, password, name})
   }
 
+  const handeSignIn = async () => {
+    await signIn({email, password})
+  }
+
+  //  skeleton loader
+
   
+
   return <div>Auth</div>;
 }
